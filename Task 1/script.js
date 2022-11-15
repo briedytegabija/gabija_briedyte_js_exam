83c50aca5ad0619b6800ae7287e4711d9764fa03
Gabija Briedyte <gabijabriedyte07@gmail.com>
2022-11-15 18:22:05 +0200

    Nusitaikem i id

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,5 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: rezultatas turi būti matomas pateikus formą ir atvaizduojamas
 <div id="output"></div> viduje. Gautus atsakymus stilizuokite naudojant CSS;
 ------------------------------------------------------------------- */
+
+

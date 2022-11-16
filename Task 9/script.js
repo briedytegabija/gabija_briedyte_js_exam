f0e770217cfd96dca1050b3bea57b8da8974a48f
Gabija Briedyte <gabijabriedyte07@gmail.com>
2022-11-16 11:42:36 +0200

    task 9 done + exports of task 10

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,28 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+
+    constructor(title, director, budget){
+        this.title = title;
+        this.director = director;
+        this.budget = budget;
+    }
+
+    wasExpensive(){
+        if (this.budget > 100000000){
+
+            return true;
+
+        }
+
+        else {
+            return false;
+        }
+    }
+}
+
+const movie = new Movie("Top Gun Maveric","Joseph Kosinski",170000000);
+
+console.log(movie.wasExpensive());

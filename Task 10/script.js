699abdc125862a1a121086c25d5933af74568a7e
Gabija Briedyte <gabijabriedyte07@gmail.com>
2022-11-16 11:52:24 +0200

    task 10 done

--- Task 10/script.js
+++ Task 10/script.js
@@ -2,6 +2,11 @@
 Sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pateiktos funkcijos.
 -------------------------------------------------------------------------------------------- */
 
+import { one, two, three, four, five } from "./modules/numbers/numbers.js";
+import composition from "./modules/math/composition.js";
+import multiplication from "./modules/math/multiplication.js";
+import division from "./modules/math/division.js";
+import substraction from "./modules/math/subtraction.js";
 
 let a = composition(one, four);
 let b = division(four, two);
